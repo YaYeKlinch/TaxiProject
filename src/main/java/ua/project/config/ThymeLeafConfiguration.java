0371c338c6067d8c3e6e7bae3248801af908f4e7
YaYeKlinch <klisandrij@gmail.com>
2021-05-14 13:48:26 +0300

    navbar

---
+++ src/main/java/ua/project/config/ThymeLeafConfiguration.java
@@ -0,0 +1,14 @@
+package ua.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+
+@Configuration
+public class ThymeLeafConfiguration {
+
+    @Bean
+    public SpringSecurityDialect springSecurityDialect(){
+        return new SpringSecurityDialect();
+    }
+}
